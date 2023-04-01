4c20deffd4846981bd251e341e3067e3203df9d0
JDunc <jjsduncan@gmail.com>
2023-03-31 21:43:20 -0400

    add password length prompt

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ var uniqueChar = ["!","@","#","$","%","^","&","*","<",">","?"]
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
+var pwordLength = "";
+
+function generatePassword() {
+console.log("hello");
+pwordLength = prompt ("Please select a password length between 8-128 characters");
+}
+
+
 
 
 var generateBtn = document.querySelector("#generate");
