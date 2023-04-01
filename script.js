931a46637f2429d086392e11ab803c148dec2b95
JDunc <jjsduncan@gmail.com>
2023-04-01 15:05:47 -0400

    added prompt functions

--- script.js
+++ script.js
@@ -1,15 +1,62 @@
 // Assignment Code
 
-var numbers = [1,2,3,4,5,6,7,8,9,0]
-var uniqueChar = ["!","@","#","$","%","^","&","*","<",">","?"]
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+var uniqueChar = ["!", "@", "#", "$", "%", "^", "&", "*", "<", ">", "?"]
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var characterPool = []
 
 var pwordLength = "";
 
 function generatePassword() {
-console.log("hello");
-pwordLength = prompt ("Please select a password length between 8-128 characters");
+  console.log("hello");
+  pwordLength = prompt("Please select a password length between 8-128 characters");
+
+  if (pwordLength >= 8 && pwordLength <= 128) {
+
+    // do you want lowercase? YES
+    var useLowercase = confirm("Include Lower Case")
+    console.log(useLowercase)
+    if(useLowercase) {
+      characterPool = characterPool.concat(lowerCase)
+    }
+    // add lowercase values to characterPool
+
+    //do you want uppercase? NO
+    var useUppercase = confirm("Include Upper Case")
+    console.log(useUppercase)
+    if(useUppercase) {
+      characterPool = characterPool.concat(upperCase)
+    }
+   
+
+    
+
+
+
+
+
+    console.log(characterPool)
+
+    // TODO: ask more questions (numeric/special chars)
+
+
+
+
+
+
+    // ......
+    // pull random characters from characterPool and create password
+    for (var i = 0; i < parseInt(pwordLength); i++) {
+      var randomIndex = Math.floor(Math.random() * characterPool.length);
+      console.log(characterPool[randomIndex])
+    }
+
+    return randomPassword;
+
+  }
+
+
 }
 
 
